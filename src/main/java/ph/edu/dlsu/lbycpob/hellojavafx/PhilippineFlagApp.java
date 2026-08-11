69b65a3ce3b0b200b6924b60575d727d9c33ca77
Serena Smith <serena_smith@dlsu.edu.ph>
2026-08-11 12:54:05 +0800

    Created drawBlueStripe, drawRedStrip, drawWhiteTriangle, drawSun, drawDiagonalRays methods.

--- src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/PhilippineFlagApp.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/PhilippineFlagApp.java
@@ -32,4 +32,43 @@ public class PhilippineFlagApp {
         drawThreeStars(x, y, width, height);
         return canvas;
     }
+    void drawBlueStripe(int x, int y, int width, int height) {
+        graphics.setFill(Color.BLUE);
+        graphics.fillRect(x, y, width, height/2);
+    }
+    void drawRedStripe(int x, int y, int width, int height) {
+        graphics.setFill(Color.RED);
+        graphics.fillRect(x, y+height/2, width, height/2);
+    }
+
+    void drawWhiteTriangle(int x, int y, int width, int height) {
+        double[] xPoints = {x, x, x + width/2};
+        double[] yPoints = {y, y + height, y + height/2};
+
+        graphics.setFill(Color.WHITE);
+        graphics.fillPolygon(xPoints, yPoints, 3);
+    }
+
+    void drawSun(int x, int y, int size) {
+        graphics.setFill(Color.GOLD);
+        graphics.fillOval(x, y, size, size);
+        double centerX = x + size / 2;
+        double centerY = y + size / 2;
+        drawDiagonalRays(centerX, centerY, size);
+    }
+
+    void drawDiagonalRays(double centerX, double centerY, double size ) {
+        double rayLength = size * 0.8;
+        double verticalLength = size * 1.2;
+        graphics.setStroke(Color.GOLD);
+        graphics.setLineWidth(3);
+
+        graphics.strokeLine(centerX, centerY - verticalLength, centerX, centerY + verticalLength);
+
+        graphics.strokeLine(centerX - verticalLength, centerY, centerX + verticalLength, centerY);
+
+        graphics.strokeLine(centerX - rayLength, centerY - rayLength, centerX + rayLength, centerY + rayLength);
+
+        graphics.strokeLine(centerX + rayLength, centerY - rayLength, centerX - rayLength, centerY + rayLength);
+    }
 }
